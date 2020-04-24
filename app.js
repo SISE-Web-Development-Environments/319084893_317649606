e62734258c92fd635080079cb283507e6b8fcdd4
seanav <sshhoonn3@gmail.com>
2020-04-24 18:04:16 +0300

    Moving points added, number of monsters added

--- app.js
+++ app.js
@@ -7,17 +7,28 @@ var numOfBalls;
 var blinkyImage=new Image();
 blinkyImage.src="blinky.png";
 var blinky=new Object();
-var inkyImage;
-var inky=new Object();
-var pinkyImage;
-var pinky=new Object();
-// var inkyImage=new Image();
-// inkyImage.src="inky.jpg";
-// var inky=new Object();
 
-// var pinkyImage=new Image();
-// pinkyImage.src="clyde.jpg";
-// var pinky=new Object();
+var inkyImage=new Image();
+inkyImage.src="inky.png";
+var inky=new Object();
+
+var pinkyImage=new Image();
+pinkyImage.src="pinky.png";
+var pinky=new Object();
+
+var clydeImage=new Image();
+clydeImage.src="clyde.png";
+var clyde=new Object();
+
+var starImage=new Image();
+starImage.src="star.png";
+var star=new Object();
+
+var snailImage=new Image();
+snailImage.src="snail.png";
+var snail=new Object();
+
+
 //endregion
 
 //region Fields
@@ -30,6 +41,7 @@ var game_time;
 var time_elapsed;
 var intervalPac;
 var intervalMon;
+var intervalSnail;
 var lastKeyPressed=0;
 var startAngle=0.15 * Math.PI;
 var endAngle=1.85*Math.PI;
@@ -38,9 +50,12 @@ var eyeEnd=-15;
 var blinkyAte=0;
 var inkyAte=0;
 var pinkyAte=0;
-var numOfMons=3;
-var packmanSpeed=220;
-var monstersSpeed=360;
+var clydeAte=0;
+var starAte=0;
+var numOfMons;
+var packmanSpeed=150;
+var monstersSpeed=280;
+var snailSpeed=4000;
 //endregion
 var maxPoints;
 var bigBall=new Object();
@@ -49,6 +64,14 @@ var smallBall=new Object();
 bigBall.code=1;
 mediumBall.code=9;
 smallBall.code=10;
+star.code=11;
+snail.code=12;
+var starEaten=false;
+var snailEaten=false;
+var candiesCounter=(smallBall.amount+mediumBall.amount+bigBall.amount);
+var lastHundreadExcceded=100;
+
+var godModeOn=false;
 // bigBall.color="#00FF00";
 // mediumBall.color="#ff0000";
 // smallBall.color="#000000";
@@ -60,6 +83,8 @@ smallBall.code=10;
 
 var boardI=10;
 var boardJ=10;
+
+
 
 $(document).ready(function() {
 	$("#optionsForm").validate({
@@ -90,18 +115,6 @@ $(document).ready(function() {
 			smallBall.amount=numOfBalls*0.6;
 			maxPoints=smallBall.amount*5+mediumBall.amount*15+bigBall.amount*25;
 
-			if(numOfMons>1)
-			{
-				inkyImage=new Image();
-				inkyImage.src="inky.jpg";
-				// inky=new Object();
-			}
-			if(numOfMons>2)
-			{
-				pinkyImage=new Image();
-				pinkyImage.src="clyde.jpg";
-				// pinky=new Object();	
-			}
 
 			$(".screen").hide();
 			$("#gameScreen").show();
@@ -130,6 +143,7 @@ function startGame()
 {
 	context = canvas.getContext("2d");
 	Start();
+
 }
 
 function Start() {
@@ -139,7 +153,6 @@ function Start() {
 	pac_color = "yellow";
 	var cnt = boardI*boardJ;
 	var food_remain = (smallBall.amount+mediumBall.amount+bigBall.amount);
-	var pacman_remain = 1;
 	start_time = new Date();
 	game_time=90000;
 
@@ -152,24 +165,32 @@ function Start() {
 			board[i][j]=0;
 		}
 	}
+
 	//insert monsters
-	board[0][0]=5;
-	blinky.i=0;
-	blinky.j=0;
-	
-	if(numOfMons>1)
-	{
-		board[0][boardJ-1]=6;
-		inky.i=0;
-		inky.j=boardJ-1;
+	for(let i=0;i<numOfMons;i++){
+		if(i==0){
+			board[0][0]=5;
+			blinky.i=0;
+			blinky.j=0;
+		}
+		else if(i==1){
+			board[0][boardJ-1]=6;
+			inky.i=0;
+			inky.j=boardJ-1;
+		}
+		else if(i==2){
+			board[boardI-1][0]=7;
+			pinky.i=boardI-1;
+			pinky.j=0;
+		}
+		else if(i==3){
+			board[boardI-1][boardJ-1]=8;
+			clyde.i=boardI-1;
+			clyde.j=boardJ-1;
+		}
 	}
-	if(numOfMons>2)
-	{
-		board[boardI-1][0]=7;
-		pinky.i=boardI-1;
-		pinky.j=0;
-	}
-	
+	//insert monsters
+
 
 	//insert walls
 
@@ -223,6 +244,18 @@ function Start() {
 		
 		food_remain--;
 	}
+
+	//insert star
+	emptyCell=findRandomEmptyCell(board);
+	board[emptyCell[0]][emptyCell[1]]=star.code;
+	star.i=emptyCell[0];
+	star.j=emptyCell[1];
+
+	//insert snail
+	emptyCell=findRandomEmptyCell(board);
+	board[emptyCell[0]][emptyCell[1]]=snail.code;
+	snail.i=emptyCell[0];
+	snail.j=emptyCell[1];
 	// ----------------------------------------------------------------------------------
 
 	keysDown = {};
@@ -242,10 +275,8 @@ function Start() {
 	);
 
 	intervalPac = setInterval(UpdatePosition, packmanSpeed);
-	// intervalMonArr[0]=setInterval(function(){ UpdateMonsterPosition(blinky,"Blinky",5); },monstersSpeed);
-	// intervalMonArr[1]=setInterval(function(){ UpdateMonsterPosition(inky,"Inky",6); },monstersSpeed);
-	// intervalMonArr[2]=setInterval(function(){ UpdateMonsterPosition(pinky,"Pinky",7); },monstersSpeed);
-	intervalMon = setInterval(UpdateMonsterPosition, monstersSpeed);
+	intervalMon = setInterval(UpdateMonsterAndStarPosition, monstersSpeed);
+	intervalSnail = setInterval(UpdateSnailPosition, snailSpeed);
 
 }
 
@@ -280,7 +311,7 @@ function Draw() {
 	canvas.width = canvas.width; //clean board
 	lblScore.value = score;
 	lblTime.value = time_elapsed;
-	lblLives.value=lives;
+	lblLives.value = lives;
 	for (var i = 0; i < boardI; i++) {
 		for (var j = 0; j < boardJ; j++) {
 			var center = new Object();
@@ -293,28 +324,31 @@ function Draw() {
 				context.fillStyle = pac_color; //color
 				context.fill();
 				context.beginPath();
-				context.arc(center.x + eyeStart, center.y +eyeEnd, 5, 0, 2 * Math.PI); // circle
+				context.arc(center.x + eyeStart, center.y + eyeEnd, 5, 0, 2 * Math.PI); // circle
 				context.fillStyle = "black"; //color
 				context.fill();
-			} 
+			}
 			else if (board[i][j] == smallBall.code) {
 				context.beginPath();
 				context.arc(center.x, center.y, 10, 0, 2 * Math.PI); // circle
 				context.fillStyle = smallBall.color; //color
 				context.fill();
-			} 
+			}
 			else if (board[i][j] == mediumBall.code) {
 				context.beginPath();
 				context.arc(center.x, center.y, 10, 0, 2 * Math.PI); // circle
 				context.fillStyle = mediumBall.color; //color
 				context.fill();
-			} 
+			}
 			else if (board[i][j] == bigBall.code) {
 				context.beginPath();
 				context.arc(center.x, center.y, 10, 0, 2 * Math.PI); // circle
 				context.fillStyle = bigBall.color; //color
 				context.fill();
-			} 
+			}
+			else if(board[i][j]==star.code){
+				context.drawImage(starImage, center.x - 30, center.y - 30, canvas.width / boardJ, canvas.height / boardI);
+			}
 			else if (board[i][j] == 4) {
 				context.beginPath();
 				context.rect(center.x - 30, center.y - 30, 60, 60);
@@ -322,13 +356,19 @@ function Draw() {
 				context.fill();
 			}
 			else if (board[i][j] == 5) {
-				context.drawImage(blinkyImage,center.x-30,center.y-30,canvas.width/boardJ,canvas.height/boardI);
+				context.drawImage(blinkyImage, center.x - 30, center.y - 30, canvas.width / boardJ, canvas.height / boardI);
 			}
 			else if (board[i][j] == 6) {
-				context.drawImage(inkyImage,center.x-30,center.y-30,canvas.width/boardJ,canvas.height/boardI);
+				context.drawImage(inkyImage, center.x - 30, center.y - 30, canvas.width / boardJ, canvas.height / boardI);
 			}
 			else if (board[i][j] == 7) {
-				context.drawImage(pinkyImage,center.x-30,center.y-30,canvas.width/boardJ,canvas.height/boardI);
+				context.drawImage(pinkyImage, center.x - 30, center.y - 30, canvas.width / boardJ, canvas.height / boardI);
+			}
+			else if (board[i][j] == 8) {
+				context.drawImage(clydeImage, center.x - 30, center.y - 30, canvas.width / boardJ, canvas.height / boardI);
+			}
+			else if (board[i][j] == snail.code) {
+				context.drawImage(snailImage, center.x - 30, center.y - 30, canvas.width / boardJ, canvas.height / boardI);
 			}
 		}
 	}
@@ -338,83 +378,79 @@ function Draw() {
 function UpdatePosition() {
 	board[shape.i][shape.j] = 0;
 	movePacman();
-	
+
 	if (board[shape.i][shape.j] == smallBall.code) {
-		score+=5;
+		score += 5;
+		candiesCounter--;
+	} else if (board[shape.i][shape.j] == mediumBall.code) {
+		score += 15;
+		candiesCounter--;
+	} else if (board[shape.i][shape.j] == bigBall.code) {
+		score += 25;
+		candiesCounter--;
 	}
-	else if (board[shape.i][shape.j] == mediumBall.code) {
-		score+=15;
-	}
-	else if (board[shape.i][shape.j] == bigBall.code) {
-		score+=25;
+	else if(board[shape.i][shape.j] == snail.code){
+		snailEaten=true;
+		window.clearInterval(intervalMon);
+		intervalMon=setInterval(UpdateMonsterAndStarPosition,monstersSpeed*6);
+		setTimeout(function () {window.clearInterval(intervalMon);
+			intervalMon=setInterval(UpdateMonsterAndStarPosition,monstersSpeed);},10000);
 	}
 	board[shape.i][shape.j] = 2;
+
+	enterGodMode();
 
 	checkCollision();
 
 
-
-
 	var currentTime = new Date();
-	time_elapsed = (game_time-(currentTime-start_time )) / 1000;
-	if(time_elapsed<=0 || score==maxPoints)
+	time_elapsed = (game_time - (currentTime - start_time)) / 1000;
+	if (time_elapsed <= 0 || score == maxPoints||candiesCounter==0)
 		endGame();
-	if (score >= 20 && time_elapsed <= 10) {
-		pac_color = "green";
-	}
-	else {
-		Draw();
-	}
+	// if (score >= 20 && time_elapsed <= 10) {
+	// 	pac_color = "green";
+	// } else {
+	// }
+	Draw();
+
  
 }
 
-function UpdateMonsterPosition(){
+function UpdateMonsterAndStarPosition(){
 
+	for(var i=0;i<numOfMons;i++){
+		if(i==0)
+			moveMonster("blinky");
+		else if(i==1)
+			moveMonster("inky");
+		else if(i==2)
+			moveMonster("pinky");
+		else if(i==3)
+			moveMonster("clyde");
+	}
 
-	board[blinky.i][blinky.j] = blinkyAte;
-	moveMonster(blinky);
-	if(board[blinky.i][blinky.j]==smallBall.code)
-		blinkyAte = smallBall.code;
-	else if(board[blinky.i][blinky.j]==mediumBall.code)
-		blinkyAte = mediumBall.code;
-	else if(board[blinky.i][blinky.j]==bigBall.code)
-		blinkyAte = bigBall.code;
-	else
-		blinkyAte = 0;
-	board[blinky.i][blinky.j] = 5;
-
-
-	board[inky.i][inky.j] = inkyAte;
-	moveMonster(inky);
-	if(board[inky.i][inky.j]==smallBall.code)
-		inkyAte = smallBall.code;
-	else if(board[inky.i][inky.j]==mediumBall.code)
-		inkyAte = mediumBall.code;
-	else if(board[inky.i][inky.j]==bigBall.code)
-		inkyAte = bigBall.code;
-	else
-		inkyAte = 0;
-	board[inky.i][inky.j] = 6;
-
-
-	board[pinky.i][pinky.j] = pinkyAte;
-	moveMonster(pinky);
-	if(board[pinky.i][pinky.j]==smallBall.code)
-		pinkyAte = smallBall.code;
-	else if(board[pinky.i][pinky.j]==mediumBall.code)
-		pinkyAte = mediumBall.code;
-	else if(board[pinky.i][pinky.j]==bigBall.code)
-		pinkyAte = bigBall.code;
-	else
-		pinkyAte = 0;
-	board[pinky.i][pinky.j] = 7;
-
-
+	if(!starEaten)
+		moveStar();
 
 	checkCollision();
 	Draw();
 
 
+
+}
+
+function UpdateSnailPosition(){
+	if (!snailEaten) {
+		var emptyCell = findRandomEmptyCell(board);
+		board[snail.i][snail.j]=0;
+		snail.i = emptyCell[0];
+		snail.j = emptyCell[1];
+		board[emptyCell[0]][emptyCell[1]] = snail.code;
+		Draw();
+	}
+	else {
+		window.clearInterval(intervalSnail);
+	}
 }
 
 function movePacman(){
@@ -461,7 +497,32 @@ function movePacman(){
 	}
 }
 
-function moveMonster(Monster){
+function moveMonster(MonsterName){
+	let Monster;
+	if(MonsterName=="blinky"){
+		board[blinky.i][blinky.j] = blinkyAte;
+		Monster=blinky;
+	}
+
+	else if(MonsterName=="inky"){
+		board[inky.i][inky.j] = inkyAte;
+		Monster=inky;
+	}
+
+	else if(MonsterName=="pinky"){
+		board[pinky.i][pinky.j] = pinkyAte;
+		Monster=pinky;
+	}
+
+	else if(MonsterName=="clyde"){
+		board[clyde.i][clyde.j] = clydeAte;
+		Monster=clyde;
+	}
+
+
+
+
+
 	let action;
 	var rnd = Math.random();
 	// 1 - down
@@ -493,21 +554,112 @@ function moveMonster(Monster){
 			action=options[0];
 		else if(rnd>0.46&&rnd<=0.92)
 			action=options[1];
-		else 
+		else
 			action=Math.floor(Math.random() * 4 + 1);
 
 	}
 
-	if(action==1 && board[Monster.i-1][Monster.j] != 4)
+	if (action == 1 && Monster.i - 1 >= 0 && board[Monster.i - 1][Monster.j] != 4)
 		Monster.i--;
-	else if(action==2  && board[Monster.i+1][Monster.j] != 4)
+	else if (action == 2 && Monster.i + 1 < boardI && board[Monster.i + 1][Monster.j] != 4)
 		Monster.i++;
-	else if(action==3 && board[Monster.i][Monster.j + 1] != 4)
+	else if (action == 3 && Monster.j + 1 < boardJ && board[Monster.i][Monster.j + 1] != 4)
 		Monster.j++;
-	else if(action==4 && board[Monster.i][Monster.j - 1] != 4)
+	else if (action == 4 && Monster.j - 1 >= 0 && board[Monster.i][Monster.j - 1] != 4)
 		Monster.j--;
 
-	
+	if(MonsterName=="blinky"){
+		// if (board[blinky.i][blinky.j] == smallBall.code)
+		// 	blinkyAte = smallBall.code;
+		// else if (board[blinky.i][blinky.j] == mediumBall.code)
+		// 	blinkyAte = mediumBall.code;
+		// else if (board[blinky.i][blinky.j] == bigBall.code)
+		// 	blinkyAte = bigBall.code;
+		// else if (board[blinky.i][blinky.j] == snail.code)
+		// 	blinkyAte = snail.code;
+		// else
+		// 	blinkyAte = 0;
+		returnEatenCandy(blinky,"blinky");
+		board[blinky.i][blinky.j] = 5;
+	}
+	else if(MonsterName=="inky"){
+		// if (board[inky.i][inky.j] == smallBall.code)
+		// 	inkyAte = smallBall.code;
+		// else if (board[inky.i][inky.j] == mediumBall.code)
+		// 	inkyAte = mediumBall.code;
+		// else if (board[inky.i][inky.j] == bigBall.code)
+		// 	inkyAte = bigBall.code;
+		// else if (board[inky.i][inky.j] == snail.code)
+		// 	inkyAte = snail.code;
+		// else
+		// 	inkyAte = 0;
+		returnEatenCandy(inky,"inky");
+		board[inky.i][inky.j] = 6;
+	}
+
+	else if(MonsterName=="pinky"){
+		// if (board[pinky.i][pinky.j] == smallBall.code)
+		// 	pinkyAte = smallBall.code;
+		// else if (board[pinky.i][pinky.j] == mediumBall.code)
+		// 	pinkyAte = mediumBall.code;
+		// else if (board[pinky.i][pinky.j] == bigBall.code)
+		// 	pinkyAte = bigBall.code;
+		// else if (board[pinky.i][pinky.j] == snail.code)
+		// 	pinkyAte = snail.code;
+		// else
+		// 	pinkyAte = 0;
+		returnEatenCandy(pinky,"pinky");
+		board[pinky.i][pinky.j] = 7;
+	}
+
+	else if(MonsterName=="clyde"){
+		// if (board[clyde.i][clyde.j] == smallBall.code)
+		// 	clydeAte = smallBall.code;
+		// else if (board[clyde.i][clyde.j] == mediumBall.code)
+		// 	clydeAte = mediumBall.code;
+		// else if (board[clyde.i][clyde.j] == bigBall.code)
+		// 	clydeAte = bigBall.code;
+		// else if (board[clyde.i][clyde.j] == snail.code)
+		// 	clydeAte = snail.code;
+		// else
+		// 	clydeAte = 0;
+		returnEatenCandy(clyde,"clyde")
+		board[clyde.i][clyde.j] = 8;
+	}
+
+
+
+}
+
+function moveStar(){
+	board[star.i][star.j] = starAte;
+
+
+	var random=Math.floor(Math.random() * 4 + 1);
+
+	if(random==1&&star.i+1<boardI&&board[star.i+1][star.j]!=4)
+		star.i++;
+	if(random==2&&star.i-1>=0&&board[star.i-1][star.j]!=4)
+		star.i--;
+	if(random==3&&star.j+1<boardJ&&board[star.i][star.j+1]!=4)
+		star.j++;
+	if(random==4&&star.j-1>=0&&board[star.i][star.j-1]!=4)
+		star.j--;
+
+
+
+	// if (board[star.i][star.j] == smallBall.code)
+	// 	starAte = smallBall.code;
+	// else if (board[star.i][star.j] == mediumBall.code)
+	// 	starAte = mediumBall.code;
+	// else if (board[star.i][star.j] == bigBall.code)
+	// 	starAte = bigBall.code;
+	// else if (board[star.i][star.j] == snail.code)
+	// 	starAte = snail.code;
+	// else
+	// 	starAte = 0;
+	returnEatenCandy(star,"star");
+	board[star.i][star.j] = star.code;
 }
 
 function randPacmanStart() {
@@ -521,39 +673,70 @@ function randPacmanStart() {
 }
 
 function checkCollision(){
-	if((blinky.i==shape.i&&blinky.j==shape.j)||(inky.i==shape.i&&inky.j==shape.j)||(pinky.i==shape.i&&pinky.j==shape.j)){
-		board[shape.i][shape.j]=0;
-		board[blinky.i][blinky.j]=0;
-		board[inky.i][inky.j]=0;
-		board[pinky.i][pinky.j]=0;
-		randPacmanStart();
-		board[blinky.i][blinky.j]=blinkyAte;
-		blinkyAte=0;
-		blinky.i=0;
-		blinky.j=0;
-		board[blinky.i][blinky.j]=5;
 
-		board[inky.i][inky.j]=inkyAte;
-		inkyAte=0;
-		inky.i=0;
-		inky.j=boardJ-1;
-		board[inky.i][inky.j]=6;
+	if (!godModeOn) {
+		let collisionFound = false;
+		for (let i = 0; i < numOfMons && !collisionFound; i++) {
+			if (i == 0) {
+				collisionFound = (blinky.i == shape.i && blinky.j == shape.j);
+			} else if (i == 1) {
+				collisionFound = (inky.i == shape.i && inky.j == shape.j);
+			} else if (i == 2) {
+				collisionFound = (pinky.i == shape.i && pinky.j == shape.j);
+			} else if (i == 3) {
+				collisionFound = (clyde.i == shape.i && clyde.j == shape.j);
+			}
+		}
 
-		board[pinky.i][pinky.j]=pinkyAte;
-		pinkyAte=0;
-		pinky.i=boardI-1;
-		pinky.j=0;
-		board[pinky.i][pinky.j]=7;
+		if (collisionFound) {
+			randPacmanStart();
+			board[shape.i][shape.j] = 0;
+			for (let i = 0; i < numOfMons; i++) {
+				if (i == 0) {
+					board[blinky.i][blinky.j] = blinkyAte;
+					blinkyAte = 0;
+					blinky.i = 0;
+					blinky.j = 0;
+					board[blinky.i][blinky.j] = 5;
+				} else if (i == 1) {
+					board[inky.i][inky.j] = inkyAte;
+					inkyAte = 0;
+					inky.i = 0;
+					inky.j = boardJ - 1;
+					board[inky.i][inky.j] = 6;
+				} else if (i == 2) {
+					board[pinky.i][pinky.j] = pinkyAte;
+					pinkyAte = 0;
+					pinky.i = boardI - 1;
+					pinky.j = 0;
+					board[pinky.i][pinky.j] = 7;
+				} else if (i == 3) {
+					board[clyde.i][clyde.j] = clydeAte;
+					clydeAte = 0;
+					clyde.i = boardI - 1;
+					clyde.j = boardJ - 1;
+					board[clyde.i][clyde.j] = 8;
+				}
+			}
+
+			if ((--lives) == 0)
+				endGame();
+			score -= 10;
 
 
-		if((--lives)==0)
-			endGame();
-		score-=10;
+			window.clearInterval(intervalPac);
+			window.clearInterval(intervalMon);
+			intervalPac = setInterval(UpdatePosition, packmanSpeed);
+			intervalMon = setInterval(UpdateMonsterAndStarPosition, monstersSpeed);
+
+		}
 	}
 
-
-
-
+	if(!starEaten&&(star.i==shape.i&&star.j==shape.j)){
+		starEaten=true;
+		score+=50;
+		enterGodMode();
+	}
 
 }
 
@@ -565,7 +748,7 @@ function endGame(){
 	{
 		alert("Game Completed!!!");
 	}
-	else if(time_elapsed<=0){
+	else if(time_elapsed<=0||candiesCounter==0){
 		if(score<100)
 			alert("You are better than "+score+" points!");
 		else
@@ -576,23 +759,83 @@ function endGame(){
 	// }
 	window.clearInterval(intervalPac);
 	window.clearInterval(intervalMon);
+	window.clearInterval(intervalSnail);
 }
 
-function chooseWho(){
-	let flag=Math.floor(Math.random() * 3) ;
-	if(flag==0)
-		UpdateMonsterPosition(blinky,"Blinky",5);
-	else if(flag==1)
-		UpdateMonsterPosition(inky,"Inky",6);
-	else if(flag==2)
-		UpdateMonsterPosition(pinky,"Plinky",7);;
+function returnEatenCandy(MovingEntity,Name){
+	var MovingEntityAte;
+	if (board[MovingEntity.i][MovingEntity.j] == smallBall.code)
+		MovingEntityAte = smallBall.code;
+	else if (board[MovingEntity.i][MovingEntity.j] == mediumBall.code)
+		MovingEntityAte = mediumBall.code;
+	else if (board[MovingEntity.i][MovingEntity.j] == bigBall.code)
+		MovingEntityAte = bigBall.code;
+	else if (board[MovingEntity.i][MovingEntity.j] == snail.code)
+		MovingEntityAte = snail.code;
+	else
+		MovingEntityAte = 0;
+
+	if(Name=="blinky")
+		blinkyAte=MovingEntityAte;
+	else if(Name=="inky")
+		inkyAte=MovingEntityAte;
+	else if(Name=="pinky")
+		pinkyAte=MovingEntityAte;
+	else if(Name=="clyde")
+		clydeAte=MovingEntityAte;
+	else if(Name=="star")
+		starAte=MovingEntityAte;
+
+
+
 }
+
+function enterGodMode(){
+	if(score>lastHundreadExcceded){
+		lastHundreadExcceded+=100;
+		godModeOn=true;
+		setTimeout(function () {godModeOn=false;pac_color="yellow";},7000);
+		pac_color="blue";
+	}
+}
+
 
 
 function handleKeys(e)
 {
 	e.preventDefault();
 	document.getElementById("upKey").value =e.keyCode;
+}
+
+function checkNotAbcentFigure(){
+	let pacmanExists=false;
+	let blinkyExists=false;
+	let inkyExists=false;
+	let pinkyExists=false;
+	let clydeExists=false;
+	for(var i=0;i<boardI;i++){
+		for(var j=0;j<boardJ;j++){
+			if(board[i][j]==2)
+				pacmanExists=true;
+			else if(board[i][j]==5)
+				blinkyExists=true;
+			else if(board[i][j]==6)
+				inkyExists=true;
+			else if(board[i][j]==7)
+				pinkyExists=true;
+			else if(board[i][j]==8)
+				clydeExists=true;
+		}
+	}
+	if(numOfMons==1)
+		return (pacmanExists&&blinkyExists);
+	else if(numOfMons==2)
+		return (pacmanExists&&blinkyExists&&inkyExists);
+	else if(numOfMons==3)
+		return (pacmanExists&&blinkyExists&&inkyExists&&pinkyExists);
+	else
+		return (pacmanExists&&blinkyExists&&inkyExists&&pinkyExists&&clydeExists);
+
 }
 
 
